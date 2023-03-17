d0ce078b7f19f1307db4e0a1106fdcb4ce47591f
Till Raab <till.raab@uni-tuebingen.de>
2023-03-17 09:53:49 +0100

    fix

--- wavetracker/gpu_harmonic_group.py
+++ wavetracker/gpu_harmonic_group.py
@@ -275,8 +275,8 @@ def get_group(freq, log_spec, spec_freqs, peaks, out, min_group_size, max_freq_t
 def get_harmonic_groups_coordinator(g_check_freqs, g_log_spec, spec_freq, peaks, out, value,
                                     min_group_size, max_freq_tol, mains_freq, mains_freq_tol):
     i, j = cuda.grid(2)
-    # if i < 1 and j < 1:
-    if i < g_check_freqs.shape[0] and j < g_check_freqs.shape[1]:
+    if i < 1 and j < 1:
+    # if i < g_check_freqs.shape[0] and j < g_check_freqs.shape[1]:
         if g_check_freqs[i, j] != 0:
             value[i, j] = get_group(g_check_freqs[i, j], g_log_spec[i], spec_freq, peaks[i], out[i, j, :],
                                     min_group_size, max_freq_tol, mains_freq, mains_freq_tol)
