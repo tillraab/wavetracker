590246caeb8a552b41010cb5a9c62ee10c6f147c
Till Raab <till.raab@uni-tuebingen.de>
2023-03-24 10:22:01 +0100

    testing

--- wavetracker/gpu_harmonic_group.py
+++ wavetracker/gpu_harmonic_group.py
@@ -495,6 +495,8 @@ def harmonic_group_pipeline(spec_arr, spec_freq_arr, cfg, verbose = 0):
     # kernel setup & execution
     tpb = (32, 32)
     bpg = (g_check_freqs.shape[0] // tpb[0] + 1, g_check_freqs.shape[1] // tpb[1] + 1)
+    embed()
+    quit()
     get_harmonic_groups_coordinator[bpg, tpb](g_check_freqs, g_log_spec, g_spec_freq, g_peaks, g_out, g_value,
                                               int64(cfg.harmonic_groups['min_group_size']),
                                               float64(cfg.harmonic_groups['max_freq_tol']),
