bb39b6c3f4c849e4307d1a2da52d966d86a11906
Till Raab <till.raab@uni-tuebingen.de>
2023-03-24 11:01:59 +0100

    fix

--- wavetracker/gpu_harmonic_group.py
+++ wavetracker/gpu_harmonic_group.py
@@ -493,7 +493,8 @@ def harmonic_group_pipeline(spec_arr, spec_freq_arr, cfg, verbose = 0):
     g_value = cuda.device_array(shape=(check_freqs.shape[0], check_freqs.shape[1]), dtype=float)
 
     # kernel setup & execution
-    tpb = 1024
+    tpb = (32, 32)
+    tpb = (1024, 1)
     bpg = (g_check_freqs.shape[0] // tpb[0] + 1, g_check_freqs.shape[1] // tpb[1] + 1)
 
     get_harmonic_groups_coordinator[bpg, tpb](g_check_freqs, g_log_spec, g_spec_freq, g_peaks, g_out, g_value,
