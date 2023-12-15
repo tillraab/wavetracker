96a97990b14a5cfc958d2db91e7546a62d43f35c
Till Raab <till.raab@uni-tuebingen.de>
2023-12-15 09:36:50 +0100

    init and version

---
+++ wavetracker/__init__.py
@@ -0,0 +1,6 @@
+"""
+Algorithms and programs for analysing and tracking electric field
+recordings of weakly electric fish.
+"""
+
+from .version import __version__
---
+++ wavetracker/version.py
@@ -0,0 +1,9 @@
+__version__='2.1.0'
+"""Current version of the wavetracker package."""
+
+__year__ = '2023'
+"""Current year for copyright messages."""
+
+__pdoc__ = {}
+__pdoc__['__version__'] = True
+__pdoc__['__year__'] = True
